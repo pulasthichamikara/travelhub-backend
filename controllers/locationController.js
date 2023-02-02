1989e19566df863fdca1780fdcb77f0c914e2663
pcchamikara <pcchamikara@gmail.com>
2023-02-02 20:46:44 +0530

    edit location done

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -35,4 +35,60 @@ module.exports = {
       res.status(500).json({ message: 'Internal server error' });
     }
   },
+  myLocations: async (req, res) => {
+    const owner = req.user._id;
+    try {
+      const myLocations = await LocationsModel.find({ owner: owner });
+      if (myLocations) {
+        res.status(200).json({ myLocations });
+      } else {
+        res.status(404).json({ message: 'You dont have Accomadations' });
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  },
+  getLocationByID: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const location = await LocationsModel.findById(id);
+      res.status(200).json({ location });
+    } catch (err) {
+      res.status(404).json({ message: 'canot find the location' });
+    }
+  },
+  editLocationByID: async (req, res) => {
+    const id = req.params.id;
+    const {
+      name,
+      address,
+      uploaderdImgs: images,
+      description,
+      perks,
+      checkin,
+      checkOut,
+      maxGuests,
+    } = req.body;
+    try {
+      const existingLocation = await LocationsModel.findById(id);
+      if (existingLocation) {
+        existingLocation.name = name || existingLocation.name;
+        existingLocation.address = address || existingLocation.address;
+        existingLocation.checkin = checkin || existingLocation.checkin;
+        existingLocation.checkOut = checkOut || existingLocation.checkOut;
+        existingLocation.maxGuests = maxGuests || existingLocation.maxGuests;
+        existingLocation.images = images || existingLocation.images;
+        existingLocation.perks = perks || existingLocation.perks;
+        existingLocation.description =
+          description || existingLocation.description;
+        const updatedLocation = await existingLocation.save();
+        res.status(200).json(updatedLocation);
+      } else {
+        res.status(404).json({ message: 'Canot find the location' });
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'something went wrong', err: err });
+    }
+  },
+  deleteLocationByID: async (req, res) => {},
 };
--- routes/locationRouter.js
+++ routes/locationRouter.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const { addLocation } = require('../controllers/locationController');
+const {
+  addLocation,
+  myLocations,
+  getLocationByID,
+  editLocationByID,
+  deleteLocationByID,
+} = require('../controllers/locationController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/', protect, addLocation);
+router.get('/myaccomadations', protect, myLocations);
+router
+  .route('/:id')
+  .get(getLocationByID)
+  .put(protect, editLocationByID)
+  .delete(protect, deleteLocationByID);
 
 module.exports = router;
