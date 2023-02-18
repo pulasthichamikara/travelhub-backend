10d991601c30a284fced7dc2b3634581f8bf4738
pcchamikara <pcchamikara@gmail.com>
2023-02-18 07:52:07 +0530

    update user login validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,49 +8,51 @@ const jwtseacret = process.env.JWT_SECRET;
 module.exports = {
   login: async (req, res) => {
     const { email, password } = req.body;
-    const userDocument = await UserModel.findOne({ email });
-    if (userDocument) {
-      const passwordOk = await bcrypt.compareSync(
-        password,
-        userDocument.password
-      );
-      if (passwordOk) {
-        jwt.sign(
-          {
-            id: userDocument._id,
-          },
-          jwtseacret,
-          {},
-          (err, token) => {
-            if (err) {
-              throw err;
-            } else {
-              res.json({
-                token: token,
-                name: userDocument.name,
-                email: userDocument.email,
-              });
-            }
-          }
-        );
-      } else {
-        res.status(401).json('password not matching');
+
+    try {
+      const userDocument = await UserModel.findOne({ email });
+
+      if (!userDocument) {
+        return res.status(401).json('Cannot find any record for this email');
       }
-    } else {
-      res.status(401).json('canot find any record for this email');
+
+      const passwordOk = await bcrypt.compare(password, userDocument.password);
+
+      if (!passwordOk) {
+        return res.status(400).json('Password not matching');
+      }
+
+      jwt.sign({ id: userDocument._id }, jwtSecret, {}, (err, token) => {
+        if (err) {
+          throw err;
+        }
+
+        res.json({
+          token: token,
+          name: userDocument.name,
+          email: userDocument.email,
+        });
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json('Something went wrong');
     }
   },
-
   register: async (req, res) => {
     const { name, email, password } = req.body;
     try {
-      const user = await UserModel.create({
-        name,
-        email,
-        password: await bcrypt.hash(password, salt),
-      });
+      const userDocument = await UserModel.findOne({ email });
+      if (userDocument) {
+        res.status(422).json({ error: 'Error while creating user' });
+      } else {
+        const user = await UserModel.create({
+          name,
+          email,
+          password: await bcrypt.hash(password, salt),
+        });
 
-      res.status(201).json(user);
+        res.status(201).json({ userId: user._id });
+      }
     } catch (e) {
       res.status(422).json({ error: 'Error while creating user' });
     }
